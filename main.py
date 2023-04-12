15326c30042a765eafd80647aca47af7aee1f2e7
wushangzh <126957096+wushangzh@users.noreply.github.com>
2023-04-12 21:06:12 +0800

    优化显示界面

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import random
 from winsound import PlaySound
-import easygui as g
+from easygui import *
 
 
 def show(U, C):
-    g.msgbox('————猜拳进行时————\n   电脑出了：%s \n   你出了：%s\n' % (C, U), '猜拳', '下一步')
+    msgbox('\t————猜拳过程————\n   电脑出了：%s \n   你出了：%s\n' % (C, U), '猜拳', '下一步')
 
 
 def competition(U, C):
@@ -12,24 +12,24 @@ def competition(U, C):
             or (U == '剪刀' and C == '布')
             or (U == '布' and C == '石头')):
         PlaySound("欧耶.wav", flags=1)
-        g.msgbox('—————结果—————\n\t你赢了！', '猜拳', '欧耶~')
+        msgbox('\t—————结果—————\n\t你赢了！', '猜拳', '欧耶~')
     elif U == C:
         PlaySound("6.wav", flags=1)
-        g.msgbox('—————结果—————\n\t平局！', '猜拳', '6')
+        msgbox('\t—————结果—————\n\t平局！', '猜拳', '6')
     else:
         PlaySound("屑.wav", flags=1)
-        g.msgbox('—————结果—————\n\t你输了！', '猜拳', '焯！')
+        msgbox('\t—————结果—————\n\t你输了！', '猜拳', '焯！')
 
 
 while True:
     P = ['石头', '剪刀', '布']
     C = random.choice(P)
-    U = g.buttonbox('请出拳：（石头、剪刀、布）', '猜拳', P, "猜拳.gif")
+    U = buttonbox('请出拳：（石头、剪刀、布）', '猜拳', P, "猜拳.gif")
 
     show(U, C)
     competition(U, C)
 
-    if g.ccbox("游戏结束！想再来一次嘛？", "提示", ["再来一次！", "退出，不玩了！"]):
+    if ccbox("游戏结束！想再来一次嘛？", "提示", ["再来一次！", "退出，不玩了！"]):
         continue
     else:
         break
